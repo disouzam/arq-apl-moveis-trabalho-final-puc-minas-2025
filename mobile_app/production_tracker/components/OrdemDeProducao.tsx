837c4b554ee6cbe9851e1462568cf5ae92edc419
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-12-14 10:38:59 -0300

    Criação de funções para o componente OrdemDeProducao para definição do estado do botão, estilo e texto exibido a depender do estado da ordem de produção

--- mobile_app/production_tracker/components/OrdemDeProducao.tsx
+++ mobile_app/production_tracker/components/OrdemDeProducao.tsx
@@ -2,8 +2,36 @@ import React from "react";
 import { Text, View } from "./Themed";
 import { StyleSheet, Pressable } from "react-native";
 import { TipoOrdemDeProducao } from "@/models/TipoOrdemDeProducao";
+import { EstadoOrdemDeProducao } from "@/models/enums/EstadoOrdemDeProducao";
 
 export default function OrdemDeProducao(ordemDeProducao: TipoOrdemDeProducao) {
+  const isButtonEnable = (estado: EstadoOrdemDeProducao) => {
+    if (estado === EstadoOrdemDeProducao.PENDENTE){
+      return false;
+    }
+    return true;
+  }
+
+  const buttonStyle = (estado: EstadoOrdemDeProducao) => {
+    if (isButtonEnable(estado)){
+      return styles.button;
+    }
+    return styles.inactiveButton;
+  }
+
+  const buttonLabel = (estado: EstadoOrdemDeProducao) => {
+    switch (estado) {
+      case EstadoOrdemDeProducao.APROVADA:
+        return "Editar";
+      case EstadoOrdemDeProducao.PENDENTE:
+        return "Solicitar aprovação";
+      case EstadoOrdemDeProducao.FINALIZADA:
+        return "Visualizar";
+      default:
+        return "Estado inconsistente";
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text
@@ -21,10 +49,10 @@ export default function OrdemDeProducao(ordemDeProducao: TipoOrdemDeProducao) {
         {ordemDeProducao?.id}
       </Text>
       <Pressable
-        style={styles.button}
+        style={buttonStyle(ordemDeProducao.state)}
         onPress={() => alert(`Deu certo!: ${ordemDeProducao.id}`)}
       >
-        <Text style={styles.buttonLabel}>Editar</Text>
+        <Text style={styles.buttonLabel}>{buttonLabel(ordemDeProducao.state)}</Text>
       </Pressable>
     </View>
   );
@@ -40,27 +68,36 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   ordemDeProducaoState: {
-    flex: 5,
+    flex: 3,
     paddingLeft: 15,
     margin: 0,
     color: "#000",
     verticalAlign: "middle"
   },
   ordemDeProducaoId: {
-    flex: 5,
+    flex: 3,
     paddingLeft: 15,
     margin: 0,
     color: "#000",
     verticalAlign: "middle"
   },
   button: {
-    flex: 2,
+    flex: 6,
     borderRadius: 0,
     padding: 15,
     margin: 0,
-    alignItems: "center",
+    alignItems: "flex-start",
     verticalAlign: "middle",
     backgroundColor: "green",
+  },
+  inactiveButton: {
+    flex: 6,
+    borderRadius: 0,
+    padding: 15,
+    margin: 0,
+    alignItems: "flex-start",
+    verticalAlign: "middle",
+    backgroundColor: "red",
   },
   buttonLabel: {
     color: "#000",
