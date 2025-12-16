4e93ac9c5e2ddb4348d39d6953e9a4699f08b763
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-12-16 18:43:08 -0300

    Métodos OnConfiguring e OnModelCreating sobrescritos na classe de contexto do banco de dados

--- backend/productionTrackerApi/Context/ProductionTrackerContext.cs
+++ backend/productionTrackerApi/Context/ProductionTrackerContext.cs
@@ -11,4 +11,14 @@ public class ProductionTrackerContext : DbContext
     public ProductionTrackerContext(DbContextOptions<ProductionTrackerContext> options) : base(options) { }
 
     public DbSet<ProductionOrder> ProductionOrders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.EnableSensitiveDataLogging();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
 }
