948f4ef19b3226fc524d999273986094ae8bc932
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-12-18 13:24:28 -0300

    Criação de um objeto de transferência de dados - Data Transfer Objects - para evitar a passagem direta de entidades / modelos para o cliente da API

---
+++ backend/productionTrackerApi/DataTransferModels/ProductionOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace productionTrackerApi.DataTransferModels;
+
+public class ProductionOrder
+{
+    public string Id { get; set; }
+
+    public List<string> StepNames { get; set; }
+
+    public List<DateTime> StartTimes { get; set; }
+
+    public List<DateTime> EndTimes { get; set; }
+
+    public ProductionOrder()
+    {
+        StepNames = new List<string>();
+        StartTimes = new List<DateTime>();
+        EndTimes = new List<DateTime>();
+    }
+}
---
+++ backend/productionTrackerApi/Extensions/ProductionOrderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace productionTrackerApi.Extensions;
+
+public static class ProductionOrderExtensions
+{
+    public static DataTransferModels.ProductionOrder ConvertToProductionOrderDto(this Models.ProductionOrder productionOrderModel)
+    {
+        var dto = new DataTransferModels.ProductionOrder();
+         dto.Id = productionOrderModel.Id;
+
+        foreach(var step in productionOrderModel.Steps) { 
+            dto.StepNames.Add(step.Name);
+
+            if (step.Start != null)
+            {
+                var startTime = (DateTime)step.Start;
+                dto.StartTimes.Add(startTime);
+            }
+
+            if(step.End != null)
+            {
+                var endTime= (DateTime)step.End;
+                dto.EndTimes.Add(endTime);
+            }
+        }
+
+        return dto;
+    }
+}
