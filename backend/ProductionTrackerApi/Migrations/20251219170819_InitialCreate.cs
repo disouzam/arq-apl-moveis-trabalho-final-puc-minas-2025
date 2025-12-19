77ce07a0df67074b3ac1760c36564867db869297
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-12-19 14:11:04 -0300

    Primeira migração criada para persistência no repositório

---
+++ backend/ProductionTrackerApi/Migrations/20251219170819_InitialCreate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductionTrackerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductionOrders",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(10)", maxLength: 10, nullable: false),
+                    State = table.Column<string>(type: "varchar(9)", unicode: false, maxLength: 9, nullable: false, defaultValue: "PENDING")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductionOrder_ProductionOrderId", x => x.Id)
+                        .Annotation("SqlServer:Clustered", false);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Steps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    Start = table.Column<DateTime>(type: "datetime", nullable: true),
+                    End = table.Column<DateTime>(type: "datetime", nullable: true),
+                    ProductionOrderId = table.Column<string>(type: "varchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Step_StepId", x => x.Id)
+                        .Annotation("SqlServer:Clustered", false);
+                    table.CheckConstraint("CK_End_After_Start", "[End] > [Start]");
+                    table.ForeignKey(
+                        name: "FK_Steps_ProductionOrders_ProductionOrderId",
+                        column: x => x.ProductionOrderId,
+                        principalTable: "ProductionOrders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Steps_ProductionOrderId",
+                table: "Steps",
+                column: "ProductionOrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Steps");
+
+            migrationBuilder.DropTable(
+                name: "ProductionOrders");
+        }
+    }
+}
