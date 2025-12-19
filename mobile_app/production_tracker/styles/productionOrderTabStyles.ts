a179df2bcf7e27192139d9c0fbbd6a667a92398a
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-12-19 16:23:50 -0300

    Rascunho de mudanças para atualizar a lista de ordens de produção

---
+++ mobile_app/production_tracker/styles/productionOrderTabStyles.ts
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+export const productionOrderTabStyles = StyleSheet.create({
+    container: {
+      display: "flex",
+      flexDirection: "row",
+      height: 50,
+      padding: 0,
+      margin: 5,
+      fontSize: 20,
+    },
+    productionOrderState: {
+      flex: 3,
+      paddingLeft: 15,
+      margin: 0,
+      color: "#000",
+      verticalAlign: "middle",
+    },
+    productionOrderId: {
+      flex: 4,
+      paddingLeft: 15,
+      margin: 0,
+      color: "#000",
+      verticalAlign: "middle",
+    },
+    button: {
+      flex: 5,
+      borderRadius: 20,
+      padding: 15,
+      margin: 0,
+      alignItems: "center",
+      verticalAlign: "middle",
+      backgroundColor: "yellow",
+    },
+    buttonLabel: {
+      color: "#000",
+      fontWeight: "bold",
+    },
+  });
